e4050a7a62964f7878b443dda819d343a581c57e
Daria Hryshchenko <daria199210@gmail.com>
2023-06-23 02:29:38 +0300

    fix deploy image

--- src/javascript/components/arena.js
+++ src/javascript/components/arena.js
@@ -85,26 +85,35 @@ function createShields(firstFighter, secondFighter) {
 }
 
 function createShield(fighter, position) {
-    const shieldImg = createImage(fighter);
+    // const shieldImg = createImage(fighter);
     const positionClassName = position === 'right' ? 'right-shield' : 'left-shield';
+    const shiel = createElement({
+        tagName: 'div',
+        className: 'shiel-wrapp'
+    });
     const shieldElement = createElement({
         tagName: 'div',
         className: `${positionClassName}`,
         attributes: { id: `${position}-shield` }
     });
-    shieldElement.append(shieldImg);
+    shiel.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" class="shiel-icon" viewBox="0 0 512 512"><path d="M256 481c-3.5 0-7-1.2-9.9-3.7-21.8-19.1-42.6-36.8-62.1-53.4C73.8 330 0 267.9 0 177.5 0 94 59 31 136 31c60.7 0 99.6 42.4 120 80.5C276.4 73.4 315.3 31 376 31c77 0 136 63 136 146.5 0 90.4-73.8 152.4-184 246.4-19.5 16.6-40.3 34.3-62.1 53.4C263 479.8 259.5 481 256 481z" fill="#FD3018"/><path d="M265.9 477.3c21.8-19.1 42.6-36.8 62.1-53.4C438.2 330 512 267.9 512 177.5 512 94 453 31 376 31c-60.7 0-99.6 42.4-120 80.5V481C259.5 481 263 479.8 265.9 477.3z" fill="#E61E14"/></svg>
+    `;
+    shieldElement.append(shiel);
+    console.log(shieldElement);
     return shieldElement;
 }
 
-export function createImage() {
-    const attributes = {
-        src: './../../../resources/shield-model.png'
-    };
-    const imgElement = createElement({
-        tagName: 'img',
-        className: 'shield-img',
-        attributes
-    });
+// export function createImage() {
+//     const attributes = {
+//         src: './../../../resources/shield-model.png',
+//         alt: 'shield'
+//     };
+//     const imgElement = createElement({
+//         tagName: 'img',
+//         className: 'shield-img',
+//         attributes
+//     });
 
-    return imgElement;
-}
+//     return imgElement;
+// }
